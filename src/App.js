a182a4963260b1717720145286411a39b1da605d
SweetArctik <juan.roseroc@campusucc.edu.co>
2025-05-22 14:22:27 -0500

    Implementacion de API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
+const API_BASE_URL = "https://qo35skdrcj.execute-api.us-east-2.amazonaws.com/prod"; //URL DEL PROD
+
 function App() {
   return (
     <div className="App">
